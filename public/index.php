980565e1b9c54c5f810684b41ba74f4bc336498d
hamidrezas <hamidreza.s@gmail.com>
2012-11-01 08:56:54 +0330

    Version 0.1.4 - Modify index.php to exclude get_include_path()

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ defined('APPLICATION_ENV')
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
+    APPLICATION_PATH.'/../library/Zend',
 )));
 
 /** Zend_Application */
